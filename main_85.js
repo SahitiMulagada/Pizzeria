23b0fabccbc17a95395db8354c5281f75fa0ff3b
Sahiti Mulagada <sahitimulagada1@gmail.com>
2022-07-15 17:20:00 +0530

    Add remove pizza feature

--- main_85.js
+++ main_85.js
@@ -5,7 +5,7 @@ function getmenu()
     var htmldata="<p style='color:#f5c71a; font-size:18px;'>Total items: " + menu_list_array.length + "</p>";
     for(var i=0; i<menu_list_array.length; i++)
     {
-        htmldata=htmldata + (i+1) + ". " + menu_list_array[i] + '<br>';
+        htmldata=htmldata + (i+1) + ". " + menu_list_array[i] + ' <button onclick="remove_pizza(' + i + ')" style="background:#e2062c;color:white;border:none;padding:2px 8px;cursor:pointer;font-size:12px;">x</button><br>';
     }
     document.getElementById("display_menu").innerHTML = htmldata;
 }
@@ -21,6 +21,11 @@ function add_suggestion(){
     else
         alert("This pizza is already on the menu!");
     document.getElementById("add_item").value="";
+    getmenu();
+}
+
+function remove_pizza(index) {
+    menu_list_array.splice(index, 1);
     getmenu();
 }
 
